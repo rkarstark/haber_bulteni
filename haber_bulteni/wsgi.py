a88966487b3aff68ee271e325b0dd2a36c374178
rkarstark <cgrgkmn@gmail.com>
2022-04-28 16:30:26 +0300

    first commit

---
+++ haber_bulteni/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for haber_bulteni project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'haber_bulteni.settings')
+
+application = get_wsgi_application()
---
+++ haberler/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from haberler.models import Makale
+# Register your models here.
+admin.site.register(Makale)
+
