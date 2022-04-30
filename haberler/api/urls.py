4af41a2f23040d04a57d4931ab72ca8b96623132
rkarstark <cgrgkmn@gmail.com>
2022-05-01 00:52:11 +0300

    finall

--- haberler/api/urls.py
+++ haberler/api/urls.py
@@ -3,9 +3,15 @@ from haberler.api import views as api_views
 
 
 urlpatterns = [
-    path('makaleler/',api_views.makale_list_create_api_view, name='makale-listesi')
-
-
-
-
+    path('yazarlar/',api_views.GazeteciListCreateAPIView.as_view(), name='yazar-listesi'),
+    path('gazeteciler/',api_views.MakaleListCreateAPIView.as_view(), name='makale-listesi'),
+    path('makaleler/<int:pk>', api_views.MakaleDetailAPIView.as_view(), name='makale-islem'),
 ]
+
+
+
+
+# urlpatterns = [
+#     path('makaleler/',api_views.makale_list_create_api_view, name='makale-listesi'),
+#     path('makaleler/<int:pk>', api_views.makale_detail_api_view, name='makale-islem'),
+# ]
--- haberler/models.py
+++ haberler/models.py
@@ -1,8 +1,19 @@
+from tkinter import CASCADE
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 
+
+class Gazeteci(models.Model):
+    isim = models.CharField(max_length=150)
+    soyisim = models.CharField(max_length=120)
+    biyografi = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.isim} {self.soyisim}'
+
 class Makale(models.Model):
-    yazar = models.CharField(max_length=150)
+    yazar = models.ForeignKey(Gazeteci, on_delete=models.CASCADE, related_name='makaleler')
     baslik = models.CharField(max_length=120)
     aciklama = models.CharField(max_length=200)
     metin = models.TextField()
